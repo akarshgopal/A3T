537e822a6bb5248462d072c1fefe3337b692c8ef
AkarshG1 <akarsh98@gmail.com>
2020-09-09 14:40:04 +0200

    wrote submodule automation script

--- submodules_update.sh
+++ submodules_update.sh
@@ -1,10 +1,10 @@
-git submodules update --init --recursive
+git submodule update --init --recursive
 cd PX4/src
 sudo rm -r Firmware
 git clone https://github.com/akarshgopal/Firmware.git
 
 cd Firmware
-git submodules update --init --recursive
+git submodule update --init --recursive
 git pull origin a3t_vau_sitl
 git checkout a3t_vau_sitl
 
@@ -13,7 +13,7 @@ sudo rm -r sitl_gazebo
 git clone https://github.com/akarshgopal/sitl_gazebo.git
 
 cd sitl_gazebo
-git submodules update --init --recursive
+git submodule update --init --recursive
 # pull and switch to sitl directory
 git pull origin a3t_vau_sitl
 git checkout a3t_vau_sitl
